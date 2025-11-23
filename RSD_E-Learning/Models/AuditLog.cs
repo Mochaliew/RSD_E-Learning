a3963f7d03cb42aec0e68f0db6893de2964a5e19
cheeluo <cheeluoolim@gmail.com>
2025-11-24 01:06:48 +0800

    add vm and  (sry for accidentally deleted the teacher controller hahaha (restore liao)))))

--- RSD_E-Learning/Models/AuditLog.cs
+++ RSD_E-Learning/Models/AuditLog.cs
@@ -4,5 +4,11 @@ namespace RSD_E_Learning.Models
 {
     public class AuditLog
     {
+        public int AuditLogId { get; set; }
+        public int? UserId { get; set; }
+        public string Action { get; set; } = "";
+        public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+        public string? Details { get; set; }
+
     }
 }
---
+++ RSD_E-Learning/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RSD_E_Learning.Models.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required, EmailAddress]
+        [Display(Name = "Email Address")]
+        public string Email { get; set; } = "";
+
+        [Required, DataType(DataType.Password)]
+        public string Password { get; set; } = "";
+
+        public bool RememberMe { get; set; }
+    }
+}
