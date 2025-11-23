56fc712355175d1aad8db34f9a2eccad15f9e416
shanyi <mochaliew@gmail.com>
2025-11-23 16:02:55 +0800

    added more teacher page and a controller ( is it a bad move ?? )

--- RSD_E-Learning/Controllers/HomeController.cs
+++ RSD_E-Learning/Controllers/HomeController.cs
@@ -23,12 +23,12 @@ namespace RSD_E_Learning.Controllers
             return View();
         }
 
-        public IActionResult Teacher()
+        public IActionResult TeacherPage()
         {
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+            [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
