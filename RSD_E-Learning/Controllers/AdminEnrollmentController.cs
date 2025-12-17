85c51fde32c57a4533e1ee4e4eb70ca4bf854c8a
cheeluo <cheeluoolim@gmail.com>
2025-12-18 01:03:56 +0800

    view enrollment and transaction

---
+++ RSD_E-Learning/Controllers/AdminEnrollmentController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RSD_E_Learning.Models;
+
+namespace RSD_E_Learning.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class AdminEnrollmentController : Controller
+    {
+        private readonly DB _db;
+
+        public AdminEnrollmentController(DB db)
+        {
+            _db = db;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var enrollments = await _db.Enrollments
+                .Include(e => e.Student).ThenInclude(s => s.User)
+                .Include(e => e.Course)
+                .OrderByDescending(e => e.EnrolledAt)
+                .ToListAsync();
+
+            return View(enrollments);
+        }
+    }
+}
---
+++ RSD_E-Learning/Controllers/AdminTransactionController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RSD_E_Learning.Models;
+
+namespace RSD_E_Learning.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class AdminTransactionController : Controller
+    {
+        private readonly DB _db;
+
+        public AdminTransactionController(DB db)
+        {
+            _db = db;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var transactions = await _db.Enrollments
+                .Include(e => e.Student).ThenInclude(s => s.User)
+                .Include(e => e.Course)
+                .Where(e => e.PaymentStatus)
+                .OrderByDescending(e => e.EnrolledAt)
+                .ToListAsync();
+
+            return View(transactions);
+        }
+    }
+}
