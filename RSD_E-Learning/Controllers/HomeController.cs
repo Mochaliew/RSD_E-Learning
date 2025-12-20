f59d8d71b941551c66a28a9a3a162906d0cc45df
cheeluo <cheeluoolim@gmail.com>
2025-12-20 18:35:47 +0800

    landing page

--- RSD_E-Learning/Controllers/HomeController.cs
+++ RSD_E-Learning/Controllers/HomeController.cs
@@ -16,10 +16,6 @@ namespace RSD_E_Learning.Controllers
 
         public IActionResult Index()
         {
-            if (User.Identity!.IsAuthenticated && User.IsInRole("Student"))
-            {
-                return RedirectToAction("Index", "StudentDashboard");
-            }
             return View();
         }
 
