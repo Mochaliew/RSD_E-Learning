30a2bffa6e4df4218cd865f41f24e87e99d9963d
767bean69 <yxyap-wp22@student.tarc.edu.my>
2025-12-20 14:52:19 +0800

    ViewAssessment

---
+++ RSD_E-Learning/ViewModels/ViewAssessmentVm.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+using System.Collections.Generic;
+using static RSD_E_Learning.Models.DB;
+
+namespace RSD_E_Learning.ViewModels
+{
+    public class ViewAssessmentVm
+    {
+        public Assessment Assessment { get; set; }
+        public List<AssessmentQuestion> Questions { get; set; }
+    }
+}
