0b7006b28f59413ed49ea8b3257fea7a34bce9d4
767bean69 <yxyap-wp22@student.tarc.edu.my>
2025-12-20 17:31:46 +0800

    Deadline of assessment

--- RSD_E-Learning/Controllers/TeacherController.cs
+++ RSD_E-Learning/Controllers/TeacherController.cs
@@ -888,7 +888,7 @@ namespace RSD_E_Learning.Controllers
                 CourseId = model.CourseId,
                 Title = model.Title,
                 TotalMarks = model.Questions.Count,
-                DeadLine = model.DeadLine
+                DeadLine = model.DeadLine.ToUniversalTime()
             };
 
             _context.Assessments.Add(assessment);
